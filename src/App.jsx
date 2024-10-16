d5600c1b067975ceed98e2ad26042f33155a0f7c
sungmin <95615086+Clariatinet@users.noreply.github.com>
2024-10-16 23:41:57 +0900

    10.16 자식 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
 import logo from './logo.svg';
+import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import './App.css';
+import { useState } from 'react';
+import data from './data.js';
 
 function App() {
+  let [shoes, setShoes] = useState(data);
   return (
     <div className='App'>
-      <h1>aa</h1>
+      <Navbar bg='dark' data-bs-theme='dark'>
+        <Container>
+          <Navbar.Brand href='#home'>Shoes' Mall</Navbar.Brand>
+          <Nav className='me-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#features'>Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className='main-bg'></div>
+      <Container>
+        <Row>
+          {shoes.map(function (a, i) {
+            return <Card shoes={shoes} i={i} />;
+          })}
+        </Row>
+      </Container>
     </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <Col>
+      <img
+        src={`https://codingapple1.github.io/shop/shoes${props.i + 1}.jpg`}
+        alt=''
+        width='80%'
+      />
+      <h4>{props.shoes[props.i].title}</h4>
+      <p>{props.shoes[props.i].price}</p>
+    </Col>
   );
 }
 
